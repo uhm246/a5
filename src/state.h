993f6500d981df906a816d4b9518b30881a72b4a
Stephanie Fok <s3fok@edu.uwaterloo.ca>
2017-12-02 17:41:20 -0500

    Updated state.h

--- src/state.h
+++ src/state.h
@@ -1,8 +1,6 @@
 #ifndef STATE_H
 #define STATE_H
 
-enum class Colour { LBlue, DBlue, Orange, Green, Red, Purple, Yellow };
-enum class Rotation { 0, 1, 2, 3 };
 enum class Status { Empty, Temp, Solid };
 enum class Move { Down, Left, Right }
 
