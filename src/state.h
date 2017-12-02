3da832f8553d45aab18e9da95cac0436353afae2
tovacinni <tovacinni@gmail.com>
2017-12-01 19:36:10 -0500

    Updated state

--- src/state.h
+++ src/state.h
@@ -1,14 +1,7 @@
 #ifndef STATE_H
 #define STATE_H
 
-enum class StateType { NewPiece, Relay, Reply };
-enum class Colour { None, Black, White };
-enum class Direction { NW, N, NE, W, E, SW, S, SE };
-
-struct State {
-  StateType type;  // See above
-  Colour colour;   // What colour was the new piece?  (NOT what is my colour)
-  Direction direction;  // In which direction from me is the new piece?
-};
+enum class Status { Empty, Temp, Solid };
+enum class Move { Down, Left, Right }
 
 #endif
