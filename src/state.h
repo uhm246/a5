6cba22ff20435abae101f93b0b1b8451d160bf86
Stephanie Fok <s3fok@edu.uwaterloo.ca>
2017-12-04 03:13:37 -0500

    added blocktype enum

--- src/state.h
+++ src/state.h
@@ -7,6 +7,7 @@ enum class Status { Empty, Temp, Solid };
 enum class Move { Down, Left, Right };
 enum class Rotate { Clockwise, Counterclockwise };
 enum class Type { I, J, L, S, Z, T, O, Single, None };
+enum class BlockType { Current, Next };
 
 struct State {
   size_t r;
